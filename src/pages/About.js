7622135c8607801b44ac2e73c2e7c2a3509d56f1
ado durak <76266634+Alginest@users.noreply.github.com>
2022-03-11 13:24:49 +0100

    Add files via upload

---
+++ src/pages/About.js
@@ -0,0 +1,19 @@
+import React from "react";
+
+const About = () => {
+  return (
+    <section className="section about-section">
+      <h1 className="section-title">about us</h1>
+      <p>
+        {" "}
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Itaque vero
+        sapiente quo numquam magnam assumenda natus tempora necessitatibus,
+        placeat quidem doloremque explicabo vel ex recusandae nisi facilis
+        dolorem officia rem commodi fugiat. Deserunt a inventore dignissimos
+        magnam amet, facere ullam.{" "}
+      </p>
+    </section>
+  );
+};
+
+export default About;
